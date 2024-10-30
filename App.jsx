5e809f283f935587bd5637d8de405feb039e7b52
pratham704 <pratham.siddannavar28@gmail.com>
2024-10-30 23:01:31 +0530

    Enable screens optimization for improved performance in App component

--- App.jsx
+++ App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
+import { enableScreens } from 'react-native-screens'; // Import enableScreens
 import PublicRoutes from './Routes/PublicRoutes';
+
+enableScreens(); // Call enableScreens here
 
 export default function App() {
   return (
